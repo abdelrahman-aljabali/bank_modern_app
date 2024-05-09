a79f00dbff923ba54e7ef4858d5e879863d046e4
abdelrahman-aljabali <abdelrahmanaljabali1@gmail.com>
2024-05-10 00:35:32 +0200

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Testimonials from "./Testimonials";
+import Stats from "./Stats";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import GetStarted from "./GetStarted";
+import Footer from "./Footer";
+import FeedbackCrad from "./FeedbackCrad";
+import CTA from "./CTA";
+import Clients from "./Clients";
+import CardDeal from "./CardDeal";
+import Button from "./Button";
+import Business from "./Business";
+import Billing from "./Billing";
+
+export {
+  Testimonials,
+  Stats,
+  Navbar,
+  Hero,
+  GetStarted,
+  Footer,
+  FeedbackCrad,
+  CTA,
+  Clients,
+  CardDeal,
+  Button,
+  Business,
+  Billing,
+};
